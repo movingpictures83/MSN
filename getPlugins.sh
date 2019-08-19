6f3138198cf525133dcc9a5fb991cfa9fc154e0d
Trevor Cickovski <tcickovs@castalia.cs.fiu.edu>
2019-08-19 16:16:03 -0400

    Script to download plugins

---
+++ getPlugins.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+git clone https://github.com/movingpictures83/CSVTrim.git ../../plugins/CSVTrim
+git clone https://github.com/movingpictures83/Spearman.git ../../plugins/Spearman
+git clone https://github.com/movingpictures83/MCL.git ../../plugins/MCL
+git clone https://github.com/movingpictures83/ClusterMeans.git ../../plugins/ClusterMeans
+git clone https://github.com/movingpictures83/ClusterStd.git ../../plugins/ClusterStd
+git clone https://github.com/movingpictures83/ClusterLeaders.git ../../plugins/ClusterLeaders
+git clone https://github.com/movingpictures83/Tightness.git ../../plugins/Tightness
+git clone https://github.com/movingpictures83/ATria.git ../../plugins/ATria
+git clone https://github.com/movingpictures83/CSVPad.git ../../plugins/CSVPad
+git clone https://github.com/movingpictures83/QGraph.git ../../plugins/QGraph
+
